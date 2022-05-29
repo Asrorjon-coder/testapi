bac35fe4b96be02fa77bacb3af575b985eb0bd9a
dazai <asrorjon.axrorovich@gmail.com>
2022-05-29 11:10:45 -0700

    1

--- route/files.js
+++ route/files.js
@@ -5,10 +5,25 @@ const jwt = require('jsonwebtoken');
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const router = express.Router();
-
+const multer = require('multer');
 // This will help us connect to the database
 //const dbo = require("../db/conn");
 
-router.get('/', (req,res) => res.send("Welcome"));
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'file')
+    },
+    filename: (req, file, cb) => {
+        console.log(file)
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({
+    storage: storage
+})
+
+router.post('/setavatar', upload.single('file'), (req, res) => {
+    res.send("file has been uploaded successfully");
+});
 
 module.exports = router;
